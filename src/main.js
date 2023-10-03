9862cd95ca54906818a857b4eb11c4dc84da5435
Susie Gordon <susieyoungchoi@gmail.com>
2023-10-03 14:18:24 -0400

    moved routes/router into routes directory

--- src/main.js
+++ src/main.js
@@ -1,20 +1,7 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory} from 'vue-router'
 import './style.css'
 import App from './App.vue'
-import Index from './views/Index.vue'
-import Login from './views/Login.vue'
+import router from './routes'
 
-// Routes 
-const routes = [
-    { path: '/', component: Index },
-    { path: '/login', component: Login }
-];
-
-// Router
-const router = createRouter({
-    history: createWebHistory(),
-    routes: routes
-});
 
 createApp(App).use(router).mount('#app')
